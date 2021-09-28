560e8ff32f053468d4cce684122fa1e6b2154594
Caprena Richie <robcaprena@gmail.com>
2021-09-28 12:31:04 -0400

    first commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Four Integer Variables
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4) 
+            Console.WriteLine($"{a}/{b} {quotient} is {remainder}");
+
+        }
+
+    }
+}
